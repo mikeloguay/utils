775cddab8a693c94e79f051d60c136afd5e5c7b3
Miguel Fernández <miguel.fernandez@frontiersin.net>
2022-06-29 15:58:59 +0200

    ES client added (not working)

---
+++ kafka-beginners-course/src/main/java/org/miguel/elastic/ElasticSearchConsumer.java
@@ -0,0 +1,63 @@
+package org.miguel.elastic;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
+
+@Slf4j
+public class ElasticSearchConsumer {
+
+    public static RestHighLevelClient createClient() {
+
+        // https://afdrreedmf:qej8cuj8fw@kafka-beginners-test-7300471039.eu-central-1.bonsaisearch.net:443
+        String hostname = "kafka-beginners-test-7300471039.eu-central-1.bonsaisearch.net";
+        String username = "afdrreedmf";
+        String password = "qej8cuj8fw";
+
+        // don't do if you run a local ES
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https")).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                }
+        );
+
+        return new RestHighLevelClient(builder);
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        RestHighLevelClient client = createClient();
+
+        String jsonString = "{\"foo\": \"bar\"}";
+
+        var indexRequest = new IndexRequest(
+                "twitter",
+                "tweets"
+        ).source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexRequest.id();
+        log.info(id);
+
+        // close the client gracefully
+        client.close();
+    }
+}

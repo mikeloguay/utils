0256a69525a931061035511199dcd69f6df676eb
Miguel Fernández <miguel.fernandez@frontiersin.net>
2022-08-04 10:27:21 +0200

    Create documents on index tweeter working!

--- kafka-beginners-course/kafka-consumer-elasticsearch/src/main/java/org/miguel/elastic/ElasticSearchConsumer.java
+++ kafka-beginners-course/kafka-consumer-elasticsearch/src/main/java/org/miguel/elastic/ElasticSearchConsumer.java
@@ -7,14 +7,13 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.xcontent.XContentType;
+import org.elasticsearch.common.xcontent.XContentType;
 
 @Slf4j
 public class ElasticSearchConsumer {
@@ -30,14 +29,9 @@ public class ElasticSearchConsumer {
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 
-        RestClientBuilder builder = RestClient.builder(
-                new HttpHost(hostname, 443, "https")).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
-                    @Override
-                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
-                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-                    }
-                }
-        );
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(
+                        httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
 
         return new RestHighLevelClient(builder);
     }
@@ -46,15 +40,14 @@ public class ElasticSearchConsumer {
     public static void main(String[] args) {
         RestHighLevelClient client = createClient();
 
-        String jsonString = "{\"foo\": \"bar\"}";
+        String jsonString = "{ \"foo\": \"bar4\" }";
 
         var indexRequest = new IndexRequest(
-                "twitter",
-                "tweets"
+                "twitter"
         ).source(jsonString, XContentType.JSON);
 
         IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexRequest.id();
+        String id = indexResponse.getId();
         log.info(id);
 
         // close the client gracefully

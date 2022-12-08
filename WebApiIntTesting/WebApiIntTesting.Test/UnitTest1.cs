2d2907ed87e53f0981380bacc516e595e78225dd
Miguel Fernández <mikeloguay@gmail.com>
2022-12-08 21:35:52 +0100

    AdventureWorks console - just packages

---
+++ WebApiIntTesting/WebApiIntTesting.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WebApiIntTesting.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

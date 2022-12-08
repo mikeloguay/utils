c0c24af0eca4aa3048dca76534fe43855c30ee53
Miguel Fernández <mikeloguay@gmail.com>
2022-12-08 22:42:17 +0100

    Datamodel generated. Insert example

--- AdventureWorksEf/AdventureWorksEf/Program.cs
+++ AdventureWorksEf/AdventureWorksEf/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventureWorksEf.Context;
+using AdventureWorksEf.DataModel;
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new AdventureWorksContext())
+{
+    //var addressTypes = await context.AddressTypes.ToListAsync();
+    //addressTypes.ForEach(a => Console.WriteLine(a.Name));
+
+    var person = await context.People.SingleAsync(p => p.BusinessEntityId == 20777);
+
+    var emailAddress = new EmailAddress
+    {
+        EmailAddress1 = "mikelus@home.test"
+    };
+
+    person.EmailAddresses.Add(emailAddress);
+    int rowsAffected = context.SaveChanges();
+
+    Console.WriteLine($"rowsAffected={rowsAffected}. EmailAddressID={emailAddress.EmailAddressId}");
+}

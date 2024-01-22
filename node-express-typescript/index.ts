6ed3e16c73f35ce494f947c5f98138e3bb4c9cab
Miguel Fernández <mikeloguay@gmail.com>
2024-01-22 20:29:21 +0100

    Call using fetch

--- node-express-typescript/index.ts
+++ node-express-typescript/index.ts
@@ -7,10 +7,18 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
+
+  await logTodo1();
 });
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
+
+async function logTodo1() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const todo = await response.json();
+  console.log(todo);
+}

ed2c3dd361534871fceac0c935c86a2684dcefdf
Miguel Fernández <miguel.fernandez@frontiersin.net>
2024-03-11 15:34:22 +0100

    First simple version

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,7 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("FizzBuzz");
+    else if (i % 3 == 0) Console.WriteLine("Fizz");
+    else if (i % 5 == 0) Console.WriteLine("Buzz");
+    else Console.WriteLine(i);
+}

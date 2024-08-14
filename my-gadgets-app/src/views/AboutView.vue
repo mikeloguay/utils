573a63d556c362b86dd4af1aab7b77eb64175684
Miguel Fernández <mikeloguay@gmail.com>
2024-08-14 19:33:11 +0200

    feat: call to API

--- my-gadgets-app/src/views/AboutView.vue
+++ my-gadgets-app/src/views/AboutView.vue
@@ -1,15 +1,82 @@
+<!--
+This example fetches the latest Vue.js commits data from GitHub’s API and displays them as a list.
+You can switch between the two branches.
+-->
+
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+
+interface Commit {
+  sha: string;
+  html_url: string;
+  author: {
+    html_url: string;
+  };
+  commit: {
+    author: {
+      name: string;
+      date: string;
+    };
+    message: string;
+  };
+}
+
+const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`
+const branches = ['main', 'v2-compat'] as const
+
+const currentBranch = ref<typeof branches[number]>(branches[0])
+const commits = ref<Commit[]>([])
+
+watchEffect(async () => {
+  // This effect will run immediately and then
+  // re-run whenever currentBranch.value changes
+  const url = `${API_URL}${currentBranch.value}`
+  const response = await fetch(url)
+  commits.value = await response.json()
+})
+
+function truncate(v: string): string {
+  const newline = v.indexOf('\n')
+  return newline > 0 ? v.slice(0, newline) : v
+}
+
+function formatDate(v: string): string {
+  return v.replace(/T|Z/g, ' ')
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input type="radio" :id="branch" :value="branch" name="branch" v-model="currentBranch">
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/vue@{{ currentBranch }}</p>
+  <ul v-if="commits.length > 0">
+    <li v-for="{ html_url, sha, author, commit } in commits" :key="sha">
+      <a :href="html_url" target="_blank" class="commit">{{ sha.slice(0, 7) }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span><br>
+      by <span class="author">
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span class="date">{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+a {
+  text-decoration: none;
+  color: #42b883;
+}
+
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+
+.author,
+.date {
+  font-weight: bold;
 }
 </style>

bd47b3c3d4c77edbb201733c7bcdad5790b68b72
Miguel Fernández <mikeloguay@gmail.com>
2025-01-25 11:30:29 +0100

    times and error handling

--- WorkerServiceSample/WorkerServiceSample/Worker.cs
+++ WorkerServiceSample/WorkerServiceSample/Worker.cs
@@ -2,22 +2,31 @@ namespace WorkerServiceSample
 {
     public class Worker : BackgroundService
     {
+        private static int _count;
         private readonly ILogger<Worker> _logger;
+        private readonly PeriodicTimer _timer;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            _timer = new(TimeSpan.FromSeconds(3));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                try
                 {
+                    if (_count++ % 5 == 0) throw new InvalidOperationException("MFG error every 5");
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    //await Task.Delay(1000, stoppingToken);
+                    await _timer.WaitForNextTickAsync(stoppingToken);
                 }
-                await Task.Delay(1000, stoppingToken);
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error in Worker");
+                }
             }
         }
     }

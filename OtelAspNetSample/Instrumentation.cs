e468ccb9f89c12e62e07699e740ffd7b18509acc
Miguel Fernández <mikeloguay@gmail.com>
2025-01-25 10:26:11 +0100

    Adding OpenTelemetry example

---
+++ OtelAspNetSample/Instrumentation.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace OtelAspNetSample;
+
+public class Instrumentation : IDisposable
+{
+    const string METER_NAME = "Mikelus.OtelAspNetSample";
+    private readonly Meter _meter;
+
+    public Instrumentation()
+    {
+        string? version = typeof(Instrumentation).Assembly.GetName().Version?.ToString();
+        _meter = new Meter(METER_NAME, version);
+        MfgCounter = _meter.CreateCounter<long>("mfg_counter", description: "The number of whatever mfgs");
+    }
+
+    public Counter<long> MfgCounter { get; }
+
+    public void Dispose()
+    {
+        _meter.Dispose();
+    }
+}
--- OtelAspNetSample/Program.cs
+++ OtelAspNetSample/Program.cs
@@ -1,24 +1,102 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Diagnostics.Metrics;
 
-var builder = WebApplication.CreateBuilder(args);
+var appBuilder = WebApplication.CreateBuilder(args);
 
-// Configure OpenTelemetry with metrics and auto-start.
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource
-        .AddService(serviceName: builder.Environment.ApplicationName))
-    .WithMetrics(metrics => metrics
-        .AddAspNetCoreInstrumentation()
-        .AddConsoleExporter((exporterOptions, metricReaderOptions) =>
+// Build a resource configuration action to set service information.
+Action<ResourceBuilder> configureResource = r => r.AddService(
+    serviceName: appBuilder.Configuration.GetValue("ServiceName", defaultValue: "otel-test")!,
+    serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+    serviceInstanceId: Environment.MachineName);
+
+Meter _meterMyLibrary = new("Mikelus.OtelAspNetSample", "1.0");
+
+// Configure OpenTelemetry tracing & metrics with auto-start using the
+// AddOpenTelemetry extension from OpenTelemetry.Extensions.Hosting.
+appBuilder.Services.AddOpenTelemetry()
+    .ConfigureResource(configureResource)
+    .WithTracing(builder =>
+    {
+        builder
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation();
+
+        // Use IConfiguration binding for AspNetCore instrumentation options.
+        appBuilder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(appBuilder.Configuration.GetSection("AspNetCoreInstrumentation"));
+
+        builder.AddOtlpExporter(otlpOptions =>
         {
-            metricReaderOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
-        }));
+            // Use IConfiguration directly for Otlp exporter endpoint option.
+            otlpOptions.Endpoint = new Uri(appBuilder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317")!);
+        });
+    })
+    .WithMetrics(builder =>
+    {
+        builder
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation();
 
-var app = builder.Build();
+        builder.AddOtlpExporter(otlpOptions =>
+        {
+            // Use IConfiguration directly for Otlp exporter endpoint option.
+            otlpOptions.Endpoint = new Uri(appBuilder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317")!);
+        });
 
-app.MapGet("/", () => $"Hello from OpenTelemetry Metrics!");
+        builder.AddMeter(_meterMyLibrary.Name);
+    });
+
+// Clear default logging providers used by WebApplication host.
+appBuilder.Logging.ClearProviders();
+
+// Configure OpenTelemetry Logging.
+appBuilder.Logging.AddOpenTelemetry(options =>
+{
+    // Note: See appsettings.json Logging:OpenTelemetry section for configuration.
+
+    var resourceBuilder = ResourceBuilder.CreateDefault();
+    configureResource(resourceBuilder);
+    options.SetResourceBuilder(resourceBuilder);
+
+    options.AddOtlpExporter(otlpOptions =>
+    {
+        // Use IConfiguration directly for Otlp exporter endpoint option.
+        otlpOptions.Endpoint = new Uri(appBuilder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317")!);
+    });
+});
+
+var counter = _meterMyLibrary.CreateCounter<int>("mfg_counter");
+
+var app = appBuilder.Build();
+
+string HandleRollDice([FromServices] ILogger<Program> logger, string? player)
+{
+    
+    
+    var result = RollDice();
+
+    if (string.IsNullOrEmpty(player))
+    {
+        logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
+    }
+    else
+    {
+        logger.LogInformation("{player} is rolling the dice: {result}", player, result);
+    }
+
+    return result.ToString(CultureInfo.InvariantCulture);
+}
+
+int RollDice()
+{
+    return Random.Shared.Next(1, 7);
+}
+
+app.MapGet("/rolldice/{player?}", HandleRollDice);
 
 app.Run();
